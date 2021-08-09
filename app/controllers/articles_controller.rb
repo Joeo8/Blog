070f3eddf59b337518c65f2d97be0b1b3e2ee599
Jolly <joe_ob@qq.com>
2021-08-09 21:11:36 +0800

    first

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,54 @@
+class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: 'root', password: '123123', except: [:index, :show]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    # render plain: params[:article].inspect
+    #<ActionController::Parameters {"title"=>"今天", "text"=>"今天是个学习的好机会"} permitted: false>
+    # @article = Article.new(params[:article])  # 健壮参数--白名单
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if  @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+end
